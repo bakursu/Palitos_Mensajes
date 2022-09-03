4a849073eae5dc10e57e031f45f5e21913634754
bakursu <jdrios.c7@gmail.com>
2022-09-03 15:26:05 -0400

    inicio del proyeto

--- app/src/main/java/com/bakursu/palitosmensajes/LoginActivity.kt
+++ app/src/main/java/com/bakursu/palitosmensajes/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.bakursu.palitosmensajes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.bakursu.palitosmensajes.databinding.ActivityLoginBinding
 import com.bakursu.palitosmensajes.databinding.ActivityMainBinding
 
@@ -17,7 +18,17 @@ class LoginActivity : AppCompatActivity() {
         binding.BtnGoRegister.setOnClickListener{
             gotoRegister()
         }
+        binding.BtnLogin.setOnClickListener{
+            login()
+        }
 
+    }
+
+    private fun login() {
+        val correo = binding.ETCorreo.text.toString()
+        val pass = binding.ETPassword.text.toString()
+
+        Log.d(tag, "login correo $correo")
     }
 
     private fun gotoRegister() {
